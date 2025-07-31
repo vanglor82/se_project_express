d7ba31b44d2189cbd68d03896c66b1bb5217bdb4
Lor Vang <vanglor82@gmail.com>
2025-07-30 19:05:57 -0500

    Project 13 added code to errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,6 +4,7 @@ const errorHandler = (err, req, res) => {
   const statusCode = err.statusCode || 500;
 
   const message = err.message || "An unexpected error occurred on the server.";
+  res.setHeader("Content-Type", "application/json");
   res.status(statusCode).json({
     message,
   });
