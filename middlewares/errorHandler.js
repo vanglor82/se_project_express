3d476ea90e4f4517cd1aba7c5b40fb5fa2a88835
Lor Vang <vanglor82@gmail.com>
2025-08-05 16:35:19 -0500

    Project 13 updates codes

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,17 +1,15 @@
-    const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
 
-      const statusCode = err.statusCode || 500;
-      const message = err.message || 'An unexpected error occurred on the server.';
+  const statusCode = err.statusCode || 500;
 
+    const message = err.message || 'An unexpected error occurred on the server.';
 
-      if (res.headersSent) {
-        return;
-      }
+  if (process.env.NODE_ENV === 'development') {
+    res.status(statusCode).json({ message: message, stack: err.stack, status: 'error' });
+  } else {
+    res.status(statusCode).json({ message: message, status: 'error' });
+  }
+};
 
-      res.setHeader('Content-Type', 'application/json');
-      res.status(statusCode).json({
-        message,
-      });
-    };
-
-    module.exports = errorHandler;
+module.exports = errorHandler;
